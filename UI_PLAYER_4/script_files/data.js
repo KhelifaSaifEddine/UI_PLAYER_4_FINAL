976b9228ca3f1adee0e34fb00545e240173c7f41
Khelifa Saif eddine <50869378+KhelifaSaifEddine@users.noreply.github.com>
2022-06-22 16:47:25 -0700

    Update data.js

--- UI_PLAYER_4/script_files/data.js
+++ UI_PLAYER_4/script_files/data.js
@@ -1,32 +1,8 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('sword_animation-right'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'Animations/swords.json'
-    
-  })
-
-  var animation1 = bodymovin.loadAnimation({
-    container: document.getElementById('sword_animation-left'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'Animations/swords.json'
-  })
-
-  var animation2 = bodymovin.loadAnimation({
-    container: document.getElementById('close_button_animation'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'Animations/close.json'
-  })
 
 
 
   var animation3 = bodymovin.loadAnimation({
-    container: document.getElementById("fire"),
+    container: document.getElementById("status_fire"),
     renderer: 'svg',
     loop: false,
     autoplay: false,
